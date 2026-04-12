93e95bfacb6365d62b9d6bb7417ad7df00abd794
Lucas <lucas.ferreira@oktz.com.br>
2026-04-12 05:48:14 -0300

    Exercicio média das alturas em arrays

---
+++ src/main/java/arrays/MediaPessoas.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MediaPessoas {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o número de alturas a serem digitadas: ");
+        int n = scanner.nextInt();
+        double[] alturas = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite a " + (i + 1) + "º altura: ");
+            alturas[i] = scanner.nextDouble();
+        }
+
+        double somaAlturas = 0;
+        for (int i = 0; i < alturas.length; i++) {
+            somaAlturas += alturas[i];
+        }
+
+        double media = somaAlturas / alturas.length;
+
+        System.out.printf("A média de altura é %.2f%n", media);
+
+        scanner.close();
+
+    }
+
+}
