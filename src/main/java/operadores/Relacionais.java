813ed79658d3057fc78f7297d86a3a7209f491f6
Lucas <lucas.ferreira@oktz.com.br>
2026-09-01 00:21:20 -0300

    Operadores relacionais

---
+++ src/main/java/operadores/Relacionais.java
@@ -0,0 +1,16 @@
+package operadores;
+
+public class Relacionais {
+
+    public static void main(String[] args) {
+
+        System.out.println(3>2);
+        System.out.println(3<2);
+        System.out.println(3>=2);
+        System.out.println(3<=2);
+        System.out.println(3!=2);
+        System.out.println(3==2);
+
+    }
+
+}
